0fcc19de14529a6ce3d85395bf4ee5c46a40d243
Dhrupad Chakraborty <dhrupadchakraborty123@gmail.com>
2024-09-11 21:42:51 +0530

    Implemented DAGsHub workflow

---
+++ README.md
@@ -0,0 +1,6 @@
+## ML FLow experiements
+
+MLFLOW_TRACKING_URI=<uri-present-in-experiments-tab> \
+MLFLOW_TRACKING_USERNAME=<User-name> \
+MLFLOW_TRACKING_PASSWORD=<access-token> \
+python script.py
--- app.py
+++ app.py
@@ -65,6 +65,10 @@ if __name__=="__main__":
         predictions=lr.predict(train_x)
         signature=infer_signature(train_x,predictions)
 
+        remote_server_uri="https://dagshub.com/dhrupad17/Understanding_MLflow_and_Dagshub.mlflow"
+
+        mlflow.set_tracking_uri(remote_server_uri)
+
         tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
         if tracking_url_type_store!="file":
             mlflow.sklearn.log_model(
